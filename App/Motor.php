30c67935fd490372323f014a896fbbe0deff411c
erfinsugiono <61791170+erfinsugiono@users.noreply.github.com>
2025-08-30 22:29:26 +0700

    Delete App/Motor.php

--- App/Motor.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App;
-
-// inheritance dari kendaraan
-class motor extends kendaraan {
-  public function pesan() {
-    echo "ini motor {$this->merk} berwarna {$this->warna} sedang berjalan."; 
-  }
-}
